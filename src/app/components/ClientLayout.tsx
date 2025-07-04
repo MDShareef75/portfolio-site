a9c4c4ddc2f3959ad29fa6019477a4bd20e24ff1
atom <jarvis@75>
2025-07-04 14:38:43 +0530

    Remove animated colorful background from layout

--- src/app/components/ClientLayout.tsx
+++ src/app/components/ClientLayout.tsx
@@ -34,13 +34,7 @@ export default function ClientLayout({
 
   return (
     <div className="min-h-screen flex flex-col relative">
-      {/* Animated background elements */}
-      <div className="fixed inset-0 z-0">
-        <div className="absolute top-0 left-0 w-full h-full bg-[url('/circuit-pattern.svg')] opacity-20"></div>
-        <div className="absolute top-1/4 -left-10 w-72 h-72 bg-blue-500/20 dark:bg-[#64ffda]/20 rounded-full blur-3xl animate-pulse-slow"></div>
-        <div className="absolute bottom-1/4 -right-10 w-72 h-72 bg-blue-500/20 dark:bg-[#64ffda]/20 rounded-full blur-3xl animate-pulse-slow delay-1000"></div>
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-96 h-96 bg-blue-500/10 dark:bg-[#64ffda]/10 rounded-full blur-3xl animate-pulse-slow delay-2000"></div>
-      </div>
+      {/* Animated background elements removed */}
 
       {/* Fixed Header */}
       <header className="fixed top-0 left-0 right-0 z-50">
